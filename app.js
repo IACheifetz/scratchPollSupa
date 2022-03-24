987c4656772c6cb4967d8ef2861c26218f72a679
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-24 14:30:05 -0700

    added signup form event listener and began work on signin form

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-// import functions and grab DOM elements
+import { signUp, signIn } from './fetch-utils.js';
 
-// let state
+const signUpForm = document.querySelector('.sign-up');
+const signInForm = document.querySelector('.sign-in');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+signUpForm.addEventListener('submit', async (e) =>{
+
+    e.preventDefault();
+
+    const data = new FormData(signUpForm);
+
+    await signUp(data.get('email'), data.get('password'));
+
+   
+
+});
